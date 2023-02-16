25eeeec7247794f0bda9e300c55f4f8a44f59a8d
Akash Rajput <akashrajput@Akashs-MacBook-Pro.local>
2023-02-16 19:56:54 +0530

    Env Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './views/dashboard/Dashboard';
+import LoginForm from './views/loginScreen/LoginForm';
+import SampleCounter from './views/SampleCounter';
+
+function App() {
+  return (
+    // <SampleCounter/>
+    // <LoginForm />
+    <Dashboard />
+  );
+}
+
+export default App;
